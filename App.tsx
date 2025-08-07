ef9b22bb4b050371dd1c71d159731dcb03975ef9
betomoedano <betomoedano@outlook.com>
2025-08-06 20:37:31 -0500

    Add currentModelId to App component state

--- App.tsx
+++ App.tsx
@@ -52,6 +52,7 @@ export default function App() {
     isInitializingModel,
     isDownloading,
     downloadProgress,
+    currentModelId,
     initializeWhisperModel,
     resetWhisperContext,
     getCurrentModel,
